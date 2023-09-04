8a705ec845bdad7612ea9eecd5b685d14973569a
Username <your@email.com>
2023-09-04 08:00:15 +0700

    First commit with assets and index.html

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+ // couter js config ( để thẻ chứa class counter để dùng)
+ $(document).ready(function () {
+    $(".counter").each(function () {
+      $(this)
+        .prop("Counter", 0)
+        .animate(
+          {
+            Counter: $(this).text(),
+          },
+          {
+            duration: 4000,
+            easing: "swing",
+            step: function (now) {
+              $(this).text(Math.ceil(now));
+            },
+          }
+        );
+    });
+  });
