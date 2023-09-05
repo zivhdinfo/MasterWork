3a6029388bbe426438c4078d2417f6ff43e06b60
Username <your@email.com>
2023-09-05 23:21:03 +0700

    update darktheme

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,21 @@
   });
 // dark theme 
 
-document.getElementById("switchButton").onclick = function () {
+/**
+Solution: 
+Khi click vào nút moon 🌙 , thẻ body sẽ được thêm class dark.
+Dựa vào class dark, ta viết css dành riêng cho dark theme.
 
-  document.getElementById("light").classList.toggle("dark");
+Ngược lại, khi click vào nút sun ☀️, ta sẽ xóa class dark để css light theme hiện lại
+
+*/
+
+//gọi thẻ button có id là switchButton
+// gắn chức năng click cho switchButton
+document.getElementById("switchButton").onclick = function () {
+  //khi button được click thì tìm đến thẻ body có id là myBody
+  // dùng classlist để gọi thuộc tính class của thẻ body
+  //Sử dụng toggle() để thêm hoặc xóa 1 class của thẻ html. Nếu thẻ body không có class dark thì toggle sẽ thêm class dark vào body. Ngược lại, nếu thẻ body đang có class dark thì toogle sẽ xóa class dark đi.
+  document.getElementById("myBody").classList.toggle("dark");
 };
+
